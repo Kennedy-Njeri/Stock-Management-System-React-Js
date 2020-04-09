dba4940d468272692db6c452c8fbaed9bad54881
Kennedy Njeri <mistakenz123@gmail.com>
2020-04-09 18:32:46 +0300

    stock and user model set up

--- auth/auth.js
+++ auth/auth.js
@@ -1,13 +1,33 @@
-const express = require('express')
-const router = express.Router()
+const jwt = require('jsonwebtoken')
+const User = require('../models/users')
+
+
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        //console.log(token)
+        const decoded = jwt.verify(token, 'thisisnodejs')
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+
+        if (!user) {
+            throw new Error()
+        }
+
+        req.token = token
+
+        // give the router handler access to the user we fetched from the database // store user fetched
+        req.user = user
+
+        // lets route handler run
+        next()
+
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate.'})
+    }
+}
 
 
 
 
-
-
-
-
-
-
-module.exports = router
+module.exports = auth
