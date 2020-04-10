5f27a0f3bc6d6ed1f7679be6c66bc33a9ad69a03
Kennedy Njeri <mistakenz123@gmail.com>
2020-04-10 18:38:32 +0300

    stock react client install and config

--- routes/stock.js
+++ routes/stock.js
@@ -5,7 +5,7 @@ const auth = require('../auth/auth')
 
 
 
-// creating a new task for a user
+// creating a new stock for a user
 router.post('/stocks', auth, async (req, res) => {
 
     const stock = new Stock({
@@ -22,24 +22,62 @@ router.post('/stocks', auth, async (req, res) => {
     
 })
 
+// // get stocks created by a user
+// router.get('/stocks', auth, async (req, res) => {
+//
+//     try {
+//         const stocks = await Stock.find({ owner: req.user._id})
+//
+//         res.send(stocks)
+//
+//     } catch (e) {
+//
+//         res.status(500).send(e)
+//
+//     }
+//
+// })
 
+
+
+// GET /stocks?limit=1
+// GET /stocks?sortBy=createdAt:desc
 router.get('/stocks', auth, async (req, res) => {
 
-    try {
-        const stocks = await Stock.find({ owner: req.user._id})
+    const sort = {}
 
-        res.send(stocks)
 
-    } catch (e) {
-
-        res.status(500).send(e)
-
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        // grabbing the value in the first array
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
+
+    try {
+
+        await req.user.populate({
+            path: 'stocks',
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                // property short hand syntax
+                sort
+            }
+        }).execPopulate()
+        res.send(req.user.stocks)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+
 
 })
 
 
 
+
+
+
+// get a specific stock created by user
 router.get('/stocks/:id', auth, async (req, res) => {
     //const _id = req.params.id
 
@@ -61,9 +99,63 @@ router.get('/stocks/:id', auth, async (req, res) => {
 })
 
 
+// edit specific stock
+router.patch('/stocks/:id', auth, async (req, res) => {
+    // convert an object to an array of its properties
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['set', 'item', 'rate', 'quantity', 'distributor']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!'})
+    }
+
+    try {
+        // find by update bypasses mongoose hence providing a direct operation(that is why we set run validators)
+        //const stock = await Stock.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        //const stock = await Stock.findById(req.params.id)
+
+        const stock = await Stock.findOne({ _id: req.params.id, owner: req.user._id})
 
 
+        if (!stock) {
+            return res.status(404).send()
+        }
 
+        updates.forEach((update) => {
+            return stock[update] = req.body[update]
+        })
+
+        // where our middleware is being executed
+        await stock.save()
+
+        res.send(stock)
+
+    } catch (e) {
+
+        res.status(400).send(e)
+    }
+})
+
+
+// delete a stock
+router.delete('/stocks/:id', auth, async (req, res) => {
+    try {
+        //const stock = await Stock.findByIdAndDelete(req.params.id)
+        const stock = await Stock.findOneAndDelete({_id: req.params.id, owner: req.user._id})
+
+        if (!stock) {
+            res.status(404).send()
+        }
+        res.send(stock)
+    } catch (e) {
+        res.status(500).send()
+    }
+
+})
 
 
 
