b8af472fa1dbc716edd08a43ad056f0c68cb2043
Kennedy Njeri <mistakenz123@gmail.com>
2020-04-10 20:06:24 +0300

    Home and About page setup

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+
+const About = () => {
+    return (
+        <div>
+            <h1>About this App</h1>
+            <p className="my-1">
+                This is a Stock management App
+            </p>
+            <p className="bg-dark p">
+               <strong>Version: 1</strong> 1.0.0
+            </p>
+
+        </div>
+    )
+}
+
+
+
+export default About
