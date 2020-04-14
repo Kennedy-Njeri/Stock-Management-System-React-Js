7231a4cefef73ad34f99fa72a1922f1d175eb517
Kennedy Njeri <mistakenz123@gmail.com>
2020-04-14 19:39:22 +0300

    register and login forms

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import React, {Fragment} from 'react';
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import StockState from './context/stock/StockState'
+import AuthState from './context/auth/AuthState'
 
 import './App.css';
 
 const App = () => {
   return (
+      <AuthState>
      <StockState>
      <Router>
     <Fragment>
@@ -18,11 +22,14 @@ const App = () => {
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route exact path='/about' component={About}/>
+          <Route exact path='/register' component={Register}/>
+          <Route exact path='/login' component={Login}/>
         </Switch>
       </div>
     </Fragment>
      </Router>
      </StockState>
+      </AuthState>
   );
 }
 
--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 //const validator = require('validator')
 
-mongoose.connect('mongodb://127.0.0.1:27018/store-api', {
+mongoose.connect('mongodb://127.0.0.1:27017/store-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
