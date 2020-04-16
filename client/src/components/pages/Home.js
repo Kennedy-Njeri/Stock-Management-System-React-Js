b5c69d3d79817d9b9e41d41d8f408dea106c1292
Kennedy Njeri <mistakenz123@gmail.com>
2020-04-16 19:01:39 +0300

    authorization fixes

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
 import Stocks from '../stock/Stocks'
 import StockForm from '../stock/StockForm'
 import StockFilter from '../stock/StockFilter'
+import AuthContext from '../../context/auth/authContext'
 
 
 
 const Home = () => {
+
+    const authContext = useContext(AuthContext)
+
+    useEffect(() => {
+        authContext.loadUser()
+        // eslint-disable-next-line 
+    }, [])
+
     return (
         <div className="grid-2">
             <div>
